11ab779f5c287f758971333ac1e2275d7b17af2d
luke student vm <luke@student.vm>
2021-07-09 19:08:58 +1000

    Implement basic key control node

---
+++ lab04_example/include/lab04_example/key_control.h
@@ -0,0 +1,52 @@
+
+#include <ros/ros.h>
+#include <termios.h>
+
+namespace KeyControl {
+    /* https://github.com/sdipendra/ros-projects/blob/master/src/keyboard_non_blocking_input/src/keyboard_non_blocking_input_node.cpp*/
+    char getch() {
+        fd_set set;
+        struct timeval timeout;
+        int rv;
+        char buff = 0;
+        int len = 1;
+        int filedesc = 0;
+        FD_ZERO(&set);
+        FD_SET(filedesc, &set);
+
+        timeout.tv_sec = 0;
+        timeout.tv_usec = 1000;
+
+        rv = select(filedesc + 1, &set, NULL, NULL, &timeout);
+
+        struct termios old = {0};
+        if (tcgetattr(filedesc, &old) < 0)
+            ROS_ERROR("tcsetattr()");
+        old.c_lflag &= ~ICANON;
+        old.c_lflag &= ~ECHO;
+        old.c_cc[VMIN] = 1;
+        old.c_cc[VTIME] = 0;
+        if (tcsetattr(filedesc, TCSANOW, &old) < 0)
+            ROS_ERROR("tcsetattr ICANON");
+
+        if(rv > 0)
+            read(filedesc, &buff, len );
+
+        old.c_lflag |= ICANON;
+        old.c_lflag |= ECHO;
+        if (tcsetattr(filedesc, TCSADRAIN, &old) < 0)
+            ROS_ERROR ("tcsetattr ~ICANON");
+        return (buff);
+    }
+
+    enum key_press {
+        NULL_KEY,
+        W_KEY,
+        A_KEY,
+        S_KEY,
+        D_KEY,
+        Q_KEY,
+        E_KEY,
+    };
+
+}
---
+++ lab04_example/src/key_input.cpp
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) 2008, Morgan Quigley and Willow Garage, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the names of Stanford University or Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <ros/ros.h>
+#include <std_msgs/Int8.h>
+
+#include <lab04_example/key_control.h>
+
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "key_input");
+
+	ros::NodeHandle n;
+
+	ros::Publisher key_control_pub = n.advertise<std_msgs::Int8>("KeyControl", 100);
+
+	auto const spin_hz = static_cast<int>(100);
+	auto const max_no_key_count = static_cast<int>(0.5*spin_hz); /* Time out every 0.5 seconds */
+
+	ros::Rate loop_rate(spin_hz);
+
+	auto key_msg = std_msgs::Int8{};
+	auto key = KeyControl::key_press{};
+
+	auto c = uint8_t{};
+	auto no_key_count = 0;
+
+	while (ros::ok())
+	{
+        c = KeyControl::getch();
+
+		switch(c) {
+			case('w'):
+				key = KeyControl::W_KEY;
+				break;
+			case('a'):
+				key = KeyControl::A_KEY;
+				break;
+			case('s'):
+				key = KeyControl::S_KEY;
+				break;
+			case('d'):
+				key = KeyControl::D_KEY;
+				break;
+			case('q'):
+				key = KeyControl::Q_KEY;
+				break;
+			case('e'):
+				key = KeyControl::E_KEY;
+				break;
+			default:
+				key = KeyControl::NULL_KEY;
+				no_key_count = ++no_key_count % max_no_key_count;
+		}
+
+		if(key != KeyControl::NULL_KEY) {
+			no_key_count = 0;
+
+			key_msg.data = key;
+			key_control_pub.publish(key_msg);
+			//ROS_INFO("Key %d", key);
+		} else {
+			if(!no_key_count){
+				/* Reached time out */
+				key_msg.data = key;
+				key_control_pub.publish(key_msg);
+				ROS_INFO("Key Control Time Out");
+			}
+		}
+
+		ros::spinOnce();
+
+		loop_rate.sleep();
+	}
+
+
+	return 0;
+}
